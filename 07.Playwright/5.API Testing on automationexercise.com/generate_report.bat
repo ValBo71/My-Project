579f1c10c4e01f08f183a98a0cb4798887fa1171
Valentin Bogdanov <vbogdanov@abv.bg>
2026-05-31 10:26:49 +0300

    Update gitignore and batch files to generate Allure reports at project root

--- 07.Playwright/5.API Testing on automationexercise.com/generate_report.bat
+++ 07.Playwright/5.API Testing on automationexercise.com/generate_report.bat
@@ -2,8 +2,8 @@
 echo ===================================================
 echo   Generating Allure Report for API Tests...
 echo ===================================================
-cd /d "%~dp0\AutomationExercise.ApiTests\bin\Debug\net9.0"
-allure generate allure-results --clean -o allure-report
+cd /d "%~dp0"
+allure generate "AutomationExercise.ApiTests\bin\Debug\net9.0\allure-results" --clean -o allure-report
 echo.
 echo Launching Allure Report server...
 allure open allure-report
--- 07.Playwright/5.API Testing on automationexercise.com/run_tests_and_report.bat
+++ 07.Playwright/5.API Testing on automationexercise.com/run_tests_and_report.bat
@@ -9,8 +9,7 @@ echo.
 echo ===================================================
 echo   Generating Allure Report...
 echo ===================================================
-cd /d "%~dp0\AutomationExercise.ApiTests\bin\Debug\net9.0"
-allure generate allure-results --clean -o allure-report
+allure generate "AutomationExercise.ApiTests\bin\Debug\net9.0\allure-results" --clean -o allure-report
 echo.
 echo Launching Allure Report server...
 allure open allure-report
